4831adc12d832ce4fc29175cf05d42ccaff3a3e4
Expedito Andrade <expeditojazz@gmail.com>
2020-08-06 15:54:16 -0300

    add president card

---
+++ src/components/PresidentCard.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+const StyledPresidentCard = styled.div.attrs({
+  className: 'w-4/5  flex justify-around bg-pink-600 mx-auto h-10 rounded align-center items-center shadow-xs opacity-90',
+})`
+  & {
+    .center {
+      ${tw`flex`}
+    }
+  }
+
+`;
+export default function PresidentCard(props) {
+  return (
+    <StyledPresidentCard>
+      <div className="left">Donald Trump</div>
+      <div className="center">
+        <div className="insta">
+          <div className="content">
+            <div className="title">Likes 900</div>
+            <div className="sub">Folowers 300</div>
+          </div>
+        </div>
+        <div className="face">
+          <div className="content">
+            <div className="title">Likes 900</div>
+            <div className="sub">Folowers 300</div>
+          </div>
+
+        </div>
+        <div className="twitter">
+          <div className="content">
+            {' '}
+            <div className="title">Likes 900</div>
+            <div className="sub">Folowers 300</div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="rigth">
+        Some content
+      </div>
+    </StyledPresidentCard>
+  );
+}
+
+// const Test = styled.div.attrs({
+//   className: 'w-full bg-purple-300 h-screen flex flex-col items-center justify-center',
+// })`
+//   & {
+//     h1 {
+//       ${tw`font-sans text-6xl bg-red-500 font-hairline text-6xl text-teal-500`}
+//     }
+//     p {
+//       ${tw`text-gray-700 text-lg`}
+//     }
+//     h2 {
+//       ${tw`text-2xl font-hairline mt-5 text-teal-500`}
+//     }
+//     ul {
+//       ${tw`inline-flex`}
+//     }
+//     li {
+//       ${tw`mr-5`}
+//     }
+//     a {
+//       ${tw`text-blue-500 hover:text-gray-500 hover:underline`}
+//     }
+//   }
+// `;
--- src/components/ShowData.js
+++ src/components/ShowData.js
@@ -4,31 +4,40 @@ import styled from 'styled-components';
 import tw from 'tailwind.macro';
 import { fetchData } from '../actionCreators/fetchData';
 import selector from '../selectors';
+import PresidentCard from './PresidentCard';
 
-const Test = styled.div.attrs({
-  className: 'w-full bg-purple-300 h-screen flex flex-col items-center justify-center',
+const PresidentIndex = styled.div.attrs({
+  className: 'w-full bg-gray-900',
 })`
-  & {
-    h1 {
-      ${tw`font-sans text-6xl bg-red-500 font-hairline text-6xl text-teal-500`}
-    }
-    p {
-      ${tw`text-gray-700 text-lg`}
-    }
-    h2 {
-      ${tw`text-2xl font-hairline mt-5 text-teal-500`}
-    }
-    ul {
-      ${tw`inline-flex`}
-    }
-    li {
-      ${tw`mr-5`}
-    }
-    a {
-      ${tw`text-blue-500 hover:text-gray-500 hover:underline`}
-    }
-  }
+ &{
+   height: 100vh;
+ }
+
 `;
+// const Test = styled.div.attrs({
+//   className: 'w-full bg-purple-300 h-screen flex flex-col items-center justify-center',
+// })`
+//   & {
+//     h1 {
+//       ${tw`font-sans text-6xl bg-red-500 font-hairline text-6xl text-teal-500`}
+//     }
+//     p {
+//       ${tw`text-gray-700 text-lg`}
+//     }
+//     h2 {
+//       ${tw`text-2xl font-hairline mt-5 text-teal-500`}
+//     }
+//     ul {
+//       ${tw`inline-flex`}
+//     }
+//     li {
+//       ${tw`mr-5`}
+//     }
+//     a {
+//       ${tw`text-blue-500 hover:text-gray-500 hover:underline`}
+//     }
+//   }
+// `;
 function ShowData(props) {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -37,11 +46,12 @@ function ShowData(props) {
   // const filtered = useSelector(state => { console.log(state); selector(state); });
   // console.log(filtered);
   return (
-    <Test>
+    <PresidentIndex>
       haha
       <h1>tailwind</h1>
       {props.socialData.info[0].profile ? props.socialData.info[0].profile : 'not yet'}
-    </Test>
+      <PresidentCard />
+    </PresidentIndex>
   );
 }
 
