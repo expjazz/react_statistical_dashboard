1aa3ea81fd0fd84e344d96bcf5b02491c64dd989
Expedito Andrade <expeditojazz@gmail.com>
2020-08-07 10:10:27 -0300

    add wikipedia img

--- src/actionCreators/fetchPresidentData.js
+++ src/actionCreators/fetchPresidentData.js
@@ -2,14 +2,36 @@
 export const fetchPresidentData = () => async (dispatch, getState) => {
   const { filter: { president } } = getState();
   try {
-    const res = await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${president}&origin=*`);
-    const json = await res.json();
-    const temp = Object.keys(json.query.pages);
-    const data = json.query.pages[temp[0]].extract;
+    let image = null;
+    const [rawText, rawImage] = await Promise.all([fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${president}&origin=*`), fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&titles=${president}&origin=*`)]);
+    const [textParsed, imageParsed] = await Promise.all([rawText.json(), rawImage.json()]);
+    const tempImage = Object.keys(imageParsed.query.pages)[0];
+    // console.log(imageParsed.query.pages[tempImage].original.source);
+    if (false) {
+      const googleImg = await fetch(`https://google-search3.p.rapidapi.com/api/v1/images/q=${president}`, {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+          'x-rapidapi-key': '3e6cc47ca5msh44e4a824e0c9feap1f89d8jsnf8d84bdce844',
+        },
+      });
+      const googleJson = await googleImg.json();
+      image = googleJson.image_results[0].image.src;
+    } else {
+      image = imageParsed.query.pages[tempImage].original.source;
+    }
+    console.log(image);
+    const temp = Object.keys(textParsed.query.pages);
+    const data = textParsed.query.pages[temp[0]].extract;
     dispatch({
-      type: 'GET_INFO_PRESIDENT', payload: data,
+      type: 'GET_INFO_PRESIDENT', payload: { text: data, image },
     });
   } catch (e) {
     console.log(e);
   }
 };
+
+// let [items, contactlist, itemgroup] = await Promise.all([
+//   fetch("http://localhost:3000/items/get"),
+//   fetch("http://localhost:3000/contactlist/get"),
+//   fetch("http://localhost:3000/itemgroup/get")
