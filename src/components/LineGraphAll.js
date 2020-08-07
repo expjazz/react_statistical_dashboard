210ca9494dbf60751089e7a1f76aadb9bcd9760f
Expedito Andrade <expeditojazz@gmail.com>
2020-08-07 19:23:41 -0300

    add fluid line chart to dashboard

--- src/components/LineGraphAll.js
+++ src/components/LineGraphAll.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import Line from 'chart.js';
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
 import { useSelector } from 'react-redux';
 import allSelects from '../selectors/allSelects';
 
 export default function LineGraphAll() {
   const { selectListsByPresidents } = allSelects;
   const rowsByPresident = useSelector(selectListsByPresidents);
-  console.log(rowsByPresident);
+  const [presidentInChart, setPresidentInChart] = useState(['Donald Trump']);
+  if (Object.keys(rowsByPresident).length === 0) {
+    return <p>loading</p>;
+  }
+
+  const setData = () => {
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const dataset = [];
+    presidentInChart.forEach(president => {
+      const cleanedData = rowsByPresident[president].map(row => row.fb_followers);
+      dataset.push({
+        data: cleanedData,
+        borderColor: 'red',
+        label: president,
+      });
+    });
+    return { labels, datasets: dataset };
+  };
+  const data = setData();
+
+  console.log(data);
   return (
-    <div>
-      line
-      {/* <Line /> */}
-    </div>
+    <Line data={data} />
   );
 }
