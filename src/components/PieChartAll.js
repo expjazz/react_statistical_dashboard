52ef63e6bfb5d066229a23b66d733692118820dc
Expedito Andrade <expeditojazz@gmail.com>
2020-08-08 15:08:30 -0300

    add change president title

--- src/components/PieChartAll.js
+++ src/components/PieChartAll.js
@@ -1,18 +1,61 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { Pie } from 'react-chartjs-2';
 import pieSelector from '../selectors/filterPieChart';
+import SelectTag from './SelectTag';
+import allSelects from '../selectors/allSelects';
 
 export default function PieChartAll() {
-  const { selectPieChartData } = pieSelector;
-  const data = useSelector(selectPieChartData);
-  console.log(data);
-
   // grab tthe data
-  // pass to chart
+  const { selectListOnlyPresidents } = allSelects;
+  const dispatch = useDispatch();
+  const presidents = useSelector(selectListOnlyPresidents);
+  const president = useSelector(state => state.pieChartAll.currentQueryPresidents[-1]);
+  const queryPresidents = useSelector(state => state.pieChartAll.currentQueryPresidents);
+  const { selectPieChartData } = pieSelector;
+  const cleanData = useSelector(selectPieChartData);
+  console.log(cleanData);
   // do event listeners to change the data
+  const handleQuery = e => {
+    queryPresidents.push(e.target.value);
+    dispatch({
+      type: 'SET_CURRENT_QUERY_PRESIDENTS_PIE',
+      payload: queryPresidents,
+    });
+  };
+  // pass to chart
+  const data = {
+    labels: ['Facebook', 'Twitter', 'Instagram'],
+    datasets: [
+      {
+        data: [cleanData.fb, cleanData.twitter, cleanData.inst],
+        backgroundColor: ['#3e95cd', '#8e5ea2', 'yellow'],
+
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    title: {
+      display: true,
+      text: queryPresidents.join(' '),
+    },
+    barValueSpacing: 20,
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 0,
+        },
+      }],
+    },
+
+  };
   return (
     <div>
+      <SelectTag value={president} content={presidents} parentState={handleQuery} />
       Hello from Pie
+      <Pie data={data} options={options} />
     </div>
   );
 }
