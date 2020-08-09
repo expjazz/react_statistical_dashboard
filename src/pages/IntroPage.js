8f2c2a6f04e91a3f4d10deecfd7628a4563f72ed
Expedito Andrade <expeditojazz@gmail.com>
2020-08-09 15:57:30 -0300

    add styling to intro page

--- src/pages/IntroPage.js
+++ src/pages/IntroPage.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
+import { Link } from 'react-router-dom';
 
 const StyledIntro = styled.div.attrs({
-  className: 'w-100 bg-red-800 flex flex-col',
+  className: 'w-100 bg-gray-300 flex flex-col content-center items-center justify-center',
 })`
-
+  height: 100vh;
+  img {
+    width: 200px;
+    height: 200px;
+  }
 `;
 
 export default function IntroPage() {
   return (
     <StyledIntro>
+      <img src="logo.png" alt="" />
       <h4>Welcome to the Liders of the World dashboard.</h4>
       <p>Here you will find data analisys about the popularity of the world leaders throughout the social media</p>
-      <button>Click here to now more</button>
+      <Link to="/index">
+
+        Click here to now more
+      </Link>
     </StyledIntro>
   );
 }
