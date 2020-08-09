0e940b291804d16df82a3ab3eb91a3acd668d478
Expedito Andrade <expeditojazz@gmail.com>
2020-08-09 15:49:10 -0300

    add stylingg to the  intro paagee

--- src/pages/IntroPage.js
+++ src/pages/IntroPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+const StyledIntro = styled.div.attrs({
+  className: 'w-100 bg-red-800 flex flex-col',
+})`
+
+`;
 
 export default function IntroPage() {
   return (
-    <div>
-      This will be the intro
-    </div>
+    <StyledIntro>
+      <h4>Welcome to the Liders of the World dashboard.</h4>
+      <p>Here you will find data analisys about the popularity of the world leaders throughout the social media</p>
+      <button>Click here to now more</button>
+    </StyledIntro>
   );
 }
