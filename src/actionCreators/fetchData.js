1b914f68978bdf7c891a3c9ac3efb8b850b6e2b0
Expedito Andrade <expeditojazz@gmail.com>
2020-08-09 17:36:30 -0300

    add correct fetch url

--- src/actionCreators/fetchData.js
+++ src/actionCreators/fetchData.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const fetchData = () => async dispatch => {
   try {
-    const res = await fetch('http://localhost:3000/facebook');
+    const res = await fetch('https://rocky-gorge-69214.herokuapp.com/facebook');
     const data = await res.json();
     dispatch({
       type: 'FETCH_DATA', payload: data,
