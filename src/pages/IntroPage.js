02916610d1288372f70dd49c66ce0809d8f23a33
Expedito Andrade <expeditojazz@gmail.com>
2020-09-03 05:51:06 -0300

    fix tailwind bug

--- src/pages/IntroPage.js
+++ src/pages/IntroPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import tw from 'tailwind.macro';
 
 const StyledIntro = styled.div.attrs({
   className: 'w-100 bg-gray-300 flex flex-col content-center items-center justify-center',
@@ -11,6 +12,11 @@ const StyledIntro = styled.div.attrs({
     width: 200px;
     height: 200px;
   }
+
+  p {
+    ${tw`px-48 text-center`}
+  }
+
 `;
 
 export default function IntroPage() {
@@ -19,11 +25,10 @@ export default function IntroPage() {
       <img src="logo.png" alt="" />
       <h4>Welcome to the Leaders of the World dashboard.</h4>
       <p>
-        Here you will find data
-        analysis about the popularity
-        of the world leaders throughout the
-        social media. The data is fake as the
-        goal of this is showcase a possible and viable react application
+        Welcome to the Leaders of the World dashboard.
+        Here you will find data analysis about the popularity of the world leaders throughout the social media. The data is fake as the goal of this is showcase a possible and viable react application
+
+        Click here to know moren
 
       </p>
       <Link to="/index">
